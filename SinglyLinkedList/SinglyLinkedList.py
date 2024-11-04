a9ae8a8496bfa07b313dffdee366403ee98e4cbf
OFAxTAB <rockenny@ymail.com>
2024-11-04 18:21:07 +0300

    add SinglyLinkedList.py add test_singly_linked_list.py

---
+++ SinglyLinkedList/SinglyLinkedList.py
@@ -0,0 +1,113 @@
+class Node:
+    __slots__ = ('data', 'next')
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+    def __str__(self):
+        return self.data
+
+    def __repr__(self):
+        return f'Node({self.data})'
+
+
+class SinglyLinkedList:
+    __slots__ = ('head', 'length')
+
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def __str__(self):
+        output = 'SinglyLinkedList('
+        current = self.head
+        if not current:
+            output += ')'
+            return output
+
+        while current.next:
+            output += f'{current.data}, '
+            current = current.next
+        output += f'{current.data}'
+        output += ')'
+        return output
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        if (index > self.length - 1) or (index < 0):
+            raise IndexError('SinglyLinkedList index out of range')
+
+        i = 0
+        current = self.head
+        while i < index:
+            current = current.next
+            i += 1
+        return current.data
+
+    def __contains__(self, item):
+        current = self.head
+        while current:
+            if current.data == item:
+                return True
+            current = current.next
+        return False
+
+    def append(self, data):
+        new_node = Node(data)
+        self.length += 1
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def insert(self, index, data):
+        if (index > self.length - 1) or (index < 0):
+            raise IndexError('SinglyLinkedList index out of range')
+        self.length += 1
+
+        if index == 0:
+            current = self.head
+            self.head = Node(data)
+            self.head.next = current
+            return
+
+        i = 0
+        prev_node = self.head
+        current_node = self.head
+        while i < index:
+            prev_node = current_node
+            current_node = current_node.next
+            i += 1
+
+        prev_node.next = Node(data)
+        prev_node.next.next = current_node
+
+    def remove(self, index):
+        if (index > self.length - 1) or (index < 0):
+            raise IndexError('SinglyLinkedList index out of range')
+        self.length -= 1
+
+        if index == 0:
+            current = self.head
+            self.head = self.head.next
+            del current
+            return
+
+        i = 0
+        prev_node = self.head
+        current_node = self.head
+        while i < index:
+            prev_node = current_node
+            current_node = current_node.next
+            i += 1
+
+        prev_node.next = current_node.next
+        del current_node
---
+++ tests/test_singly_linked_list.py
@@ -0,0 +1,87 @@
+import pytest
+
+from SinglyLinkedList import SinglyLinkedList
+
+
+def test_create():
+    llst = SinglyLinkedList()
+    assert str(llst) == 'SinglyLinkedList()'
+
+
+def test_append():
+    llst = SinglyLinkedList()
+    for i in range(10):
+        llst.append(i)
+    assert str(llst) == 'SinglyLinkedList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)'
+    llst = SinglyLinkedList()
+    llst.append(None)
+    llst.append(2.4)
+    llst.append('test')
+    assert str(llst) == 'SinglyLinkedList(None, 2.4, test)'
+
+
+def test_len():
+    llst = SinglyLinkedList()
+    assert len(llst) == 0
+    for i in range(10):
+        llst.append(i)
+    assert len(llst) == 10
+
+
+def test_get():
+    llst = SinglyLinkedList()
+    for i in range(10):
+        llst.append(i)
+    assert llst[0] == 0
+    assert llst[5] == 5
+    assert llst[9] == 9
+    with pytest.raises(IndexError):
+        llst[10]
+    with pytest.raises(IndexError):
+        llst[45]
+    with pytest.raises(IndexError):
+        llst[-1]
+
+
+def test_insert():
+    llst = SinglyLinkedList()
+    for i in range(3):
+        llst.append(i)
+    llst.insert(0, 9)
+    assert str(llst) == 'SinglyLinkedList(9, 0, 1, 2)'
+    llst.insert(1, 9)
+    assert str(llst) == 'SinglyLinkedList(9, 9, 0, 1, 2)'
+    llst.insert(4, 9)
+    assert str(llst) == 'SinglyLinkedList(9, 9, 0, 1, 9, 2)'
+    with pytest.raises(IndexError):
+        llst.insert(10, 9)
+    with pytest.raises(IndexError):
+        llst.insert(-1, 9)
+
+
+def test_delete():
+    llst = SinglyLinkedList()
+    for i in range(10):
+        llst.append(i)
+    llst.remove(0)
+    assert str(llst) == 'SinglyLinkedList(1, 2, 3, 4, 5, 6, 7, 8, 9)'
+    llst.remove(2)
+    assert str(llst) == 'SinglyLinkedList(1, 2, 4, 5, 6, 7, 8, 9)'
+    llst.remove(7)
+    assert str(llst) == 'SinglyLinkedList(1, 2, 4, 5, 6, 7, 8)'
+    with pytest.raises(IndexError):
+        llst.remove(10)
+
+
+def test_contains():
+    llst = SinglyLinkedList()
+    for i in range(10):
+        llst.append(i)
+    llst.append('qwerty')
+    llst.append(0.4)
+    assert 0 in llst
+    assert 9 in llst
+    assert 'qwerty' in llst
+    assert 0.4 in llst
+    assert 66 not in llst
+    assert -1 not in llst
